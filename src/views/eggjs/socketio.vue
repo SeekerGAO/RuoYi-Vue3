58e21fa537fdfdb75d053f68758199b3a29b5e2d
SeekerGAO <1532584997@qq.com>
2023-11-03 01:43:46 +0800

    feat: 聊天室

--- src/views/eggjs/socketio.vue
+++ src/views/eggjs/socketio.vue
@@ -1,6 +1,13 @@
 <template>
 	<div class="socketio">
-		<span>socketio</span>
+		<p @click="handleClickJoinAdmin">管理员加入</p>
+		<p @click="handleClickJoinA">A加入</p>
+		<p @click="handleClickJoinB">B加入</p>
+		<p @click="handleClickJoinC">C加入</p>
+		<p @click="handleClickAdmin">管理员</p>
+		<p @click="handleClickA">这是A</p>
+		<p @click="handleClickB">这是B</p>
+		<p @click="handleClickC">这是C</p>
 	</div>
 </template>
 
@@ -17,11 +24,42 @@ socket.on("res", (data) => {
 	console.log("res", data);
 });
 
+socket.on("msg", (data) => {
+	console.log("msg", data);
+});
+
 socket.on("disconnect", () => {
 	console.log("socket.io disconnect success");
 });
 
 socket.emit("chat", "Hello Server");
+// socket.emit("join");
+
+const handleClickJoinAdmin = () => {
+	socket.emit("join", "ADMIN");
+};
+const handleClickJoinA = () => {
+	socket.emit("join", "A");
+};
+const handleClickJoinB = () => {
+	socket.emit("join", "B");
+};
+const handleClickJoinC = () => {
+	socket.emit("join", "C");
+};
+
+const handleClickAdmin = () => {
+	socket.emit("message", { userId: "ADMIN", msg: "我是管理员" });
+};
+const handleClickA = () => {
+	socket.emit("message", { userId: "A", msg: "我是A" });
+};
+const handleClickB = () => {
+	socket.emit("message", { userId: "B", msg: "我是B" });
+};
+const handleClickC = () => {
+	socket.emit("message", { userId: "C", msg: "我是C" });
+};
 </script>
 
 <style lang="scss" scoped></style>
