820f7c2aae71e24add7c8b02746dcb0580989774
SeekerGAO <1532584997@qq.com>
2023-11-03 17:45:38 +0800

    feat: 聊天室

--- src/views/eggjs/socketio.vue
+++ src/views/eggjs/socketio.vue
@@ -1,20 +1,41 @@
 <template>
-	<div class="socketio">
-		<p @click="handleClickJoinAdmin">管理员加入</p>
-		<p @click="handleClickJoinA">A加入</p>
-		<p @click="handleClickJoinB">B加入</p>
-		<p @click="handleClickJoinC">C加入</p>
-		<p @click="handleClickAdmin">管理员</p>
-		<p @click="handleClickA">这是A</p>
-		<p @click="handleClickB">这是B</p>
-		<p @click="handleClickC">这是C</p>
+	<div class="chat-container">
+		<el-row>
+			<el-col>
+				<p v-for="item in joinedMsgList">{{ item }}</p>
+				<el-form size="large">
+					<div class="flex">
+						<el-select v-model="userSelected" placeholder="请选择发送对象">
+							<el-option v-for="item in userOptions" :key="item.userId" :label="item.userId" :value="item.userId" />
+						</el-select>
+						<el-input placeholder="发送聊天消息" v-model="sendMsg" clearable> </el-input>
+						<el-button type="primary" @click="handleClickSendMsg">发送</el-button>
+					</div>
+				</el-form>
+				<div>
+					<p v-for="item in msgList" :class="{ 'text-r': item.userId === 'OWN' }">
+						<span>{{ item.userId }} : {{ item.msg }}</span>
+					</p>
+				</div>
+			</el-col>
+		</el-row>
 	</div>
 </template>
 
 <script setup>
 import io from "socket.io-client";
+import { useRoute } from "vue-router";
+import { ref } from "vue";
 
 const socket = io("http://localhost:7001");
+
+const route = useRoute();
+const userId = route.query.userId.toLocaleUpperCase();
+const joinedMsgList = ref([]);
+const sendMsg = ref("");
+const msgList = ref([]);
+const userSelected = ref("");
+const userOptions = ref([{ userId: "ALL" }]);
 
 socket.on("connect", () => {
 	console.log("socket.io connect success");
@@ -24,7 +45,13 @@ socket.on("res", (data) => {
 	console.log("res", data);
 });
 
+socket.on("joined", (data) => {
+	userOptions.value.push({ userId: data });
+	joinedMsgList.value.push(`${data} has joined the room`);
+});
+
 socket.on("msg", (data) => {
+	msgList.value.push(data);
 	console.log("msg", data);
 });
 
@@ -33,34 +60,37 @@ socket.on("disconnect", () => {
 });
 
 socket.emit("chat", "Hello Server");
-// socket.emit("join");
 
-const handleClickJoinAdmin = () => {
+// 用户加入房间
+if (userId === "ADMIN") {
 	socket.emit("join", "ADMIN");
-};
-const handleClickJoinA = () => {
+}
+if (userId === "A") {
 	socket.emit("join", "A");
-};
-const handleClickJoinB = () => {
+}
+if (userId === "B") {
 	socket.emit("join", "B");
-};
-const handleClickJoinC = () => {
+}
+if (userId === "C") {
 	socket.emit("join", "C");
-};
+}
 
-const handleClickAdmin = () => {
-	socket.emit("message", { userId: "ADMIN", msg: "我是管理员" });
-};
-const handleClickA = () => {
-	socket.emit("message", { userId: "A", msg: "我是A" });
-};
-const handleClickB = () => {
-	socket.emit("message", { userId: "B", msg: "我是B" });
-};
-const handleClickC = () => {
-	socket.emit("message", { userId: "C", msg: "我是C" });
+const handleClickSendMsg = () => {
+	if (!sendMsg.value) return;
+	msgList.value.push({ userId: "OWN", msg: sendMsg.value });
+	socket.emit("message", { userId: userId, msg: sendMsg.value });
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.chat-container {
+	padding: 50px;
+}
+.flex {
+	display: flex;
+}
+.text-r {
+	text-align: right;
+}
+</style>
 vu
